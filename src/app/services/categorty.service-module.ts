ffff5f683d203ee8cdbc197712214db5278c228b
dawidlabarzewski <154749@student.uwm.edu.pl>
2022-11-26 16:31:54 +0100

    feat: list of categories implemented

---
+++ src/app/services/categorty.service-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CategortyService } from './categorty.service';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [CategortyService],
+  exports: []
+})
+export class CategortyServiceModule {
+}
